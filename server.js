b70796f3a7c840ba945ae6111a311b590b104b60
NicolasMarqui <nicolas.marqui@fatec.sp.gov.br>
2019-03-06 09:22:46 -0300

    path

--- server.js
+++ server.js
@@ -3,16 +3,15 @@ const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 app.use(cors());
-
-let options = {
-    server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
-    replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
-  };
-
 const db = require('./config/keys').MONGO_URI;
 const Alugar = require('./routes/api/Alugar');
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
 
 app.use(bodyParser.json());
 app.use('/api/casas/', Alugar);
@@ -22,6 +21,10 @@ mongoose.connect(db, {useNewUrlParser: true})
     .catch(err => console.log(err));
 
 const port = process.env.PORT || 5000;
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port , console.log(`Server rodando na porta ${port}`))
 
